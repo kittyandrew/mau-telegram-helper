975f1007039915c9065a38e526267c7a0e7cbaa5
andrebcd4 <andrebc375@gmail.com>
2020-01-28 03:36:07 +0200

    sample-config

---
+++ sample-config.py
@@ -0,0 +1,44 @@
+# Main account information:
+# - Main_username is a name of telethon's
+#   session name and connection name, which
+#   you can see in your account's sessions
+#   (official telegram client)
+# - Api id and hash are requirements for telethon
+# - Phone number allowes you to skip 'entering phone'
+#   step when creating session **(you may leave it as None
+#   here, if you wish)**
+Main_phone = ""
+Main_username = ""
+Main_api_id = ""
+Main_api_hash = ""
+
+Second_phone = ""
+Second_username = ""
+Second_api_id = ""
+Second_api_hash = ""
+
+# Channel id has '-100' prefix: e.g.
+ERROR_LOG_CHANNEL_ID = -100123456789
+CHANNEL_FOR_NOTES = -100123456789
+
+# Link to voice generating api
+# TODO: sophisticated voice synthesize model
+# TODO: and publically open api
+VOICE_API_URL = ""
+
+# YouTube API developer key
+YOUTUBE_API_KEY = ""
+
+BashBot_name = ""
+BashBot_token = ""
+Bot_tag = ""
+
+wolframalpha_id = ""
+
+google_creds = {}
+
+lyrebirds_id = ""
+lyrebirds_secret = ""
+
+# Bio Pattern
+BIO_PATTERN = ""
